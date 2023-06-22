e0a85a9f1f24f90f11929708916235d2f9249592
aneesazc <anees.azc@gmail.com>
2023-06-22 20:32:12 +0100

    add env and start commands

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.post("/work", function(req, res) {
 });
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000");
 });
 
